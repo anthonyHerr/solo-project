c86f0602051290cbac874cad420bb68bd9a64bc8
anthonyHerr <103393401+anthonyHerr@users.noreply.github.com>
2024-02-14 19:56:40 -0800

    started userController

--- server/controlllers/userController.js
+++ server/controlllers/userController.js
@@ -1,1 +1,27 @@
 const User = require('./model/userModel.js')
+
+const userController = {
+  // Create a new user in the Database
+  // Their information will be sent in the request body
+  // This should send the created user
+  createUser(req, res, next) {
+    const newUser = new User(req.body);
+    newUser.save((err, user) => {
+      if (err) {
+        return next({
+          log: 'Error creating user',
+          status: 500,
+          message: {err: 'Error creating user'},
+        });
+      }
+      else {
+        res.status(200).json(student)
+      }
+    })
+
+
+  },
+
+}
+
+module.exports = userController;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
-
+app.use(express.json());
 app.use('/bundle.js', express.static(path.join(__dirname, '../build/bundle.js')));
 
 
@@ -11,6 +11,16 @@ app.get('/*', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(3000, () => console.log('Listening on port 3000')); 
